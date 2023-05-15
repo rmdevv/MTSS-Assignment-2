865c12145a0809b6bf8a3f53f0412a4074efb4f4
GiulioBotta <giuliobotta02@gmail.com>
2023-05-15 22:37:45 +0200

    Sviluppo codice sorgente della classe RomanPrinter close #8

--- src/main/java/it/unipd/mtss/RomanPrinter.java
+++ src/main/java/it/unipd/mtss/RomanPrinter.java
@@ -5,13 +5,94 @@
 
 package it.unipd.mtss;
 
+import java.util.HashMap;
+
 public class RomanPrinter {
-    public static String print(int num) {
-        return printAsciiArt(IntegerToRoman.convert(num));
+
+    private static HashMap<Character, String[]> asciiart = null;
+    static {
+        asciiart = new HashMap<Character, String[]>();
+        asciiart.put('I', new String[] {
+            " _____ ",
+            "|_   _|",
+            "  | |  ",
+            "  | |  ",
+            " _| |_ ",
+            "|_____|"
+        });
+            asciiart.put('V', new String[] {
+            "__      __",
+            "\\ \\    / /",
+            " \\ \\  / / ",
+            "  \\ \\/ /  ",
+            "   \\  /   ",
+            "    \\/    "
+        });
+            asciiart.put('X', new String[] {
+            "__    __",
+            "\\ \\  / /",
+            " \\ \\/ / ",
+            "  >  <  ",
+            " / /\\ \\ ",
+            "/_/  \\_\\"
+        });
+            asciiart.put('L', new String[] {
+            " _      ",
+            "| |     ",
+            "| |     ",
+            "| |     ",
+            "| |____ ",
+            "|______|"
+        });
+            asciiart.put('C', new String[] {
+            "  _____ ",
+            " / ____|",
+            "| |     ",
+            "| |     ",
+            "| |____ ",
+            " \\_____|"
+        });
+            asciiart.put('D', new String[] {
+            " _____  ",
+            "|  __ \\ ",
+            "| |  | |",
+            "| |  | |",
+            "| |__| |",
+            "|_____/ "
+        });
+            asciiart.put('M', new String[] {
+            " __  __",
+            "|  \\/  |",
+            "| \\  / |",
+            "| |\\/| |",
+            "| |  | |",
+            "|_|  |_|"
+        });
     }
 
-    private static String printAsciiArt(String romanNumber) {
-        // TODO
-        return null;
+    public static String print(int num) throws UnexpectedNumberException, UnexpectedRomanException {
+        return printAsciiArt(IntegerToRoman.convert(num));
+    }
+  
+    private static String printAsciiArt(String romanNumber) throws UnexpectedRomanException {
+
+        StringBuilder romanAsciiart = new StringBuilder();
+
+        if(romanNumber != ""){
+            for (int i = 0; i < 6; i++) {
+                for (int j = 0; j < romanNumber.length(); j++) {
+                    char currentChar = romanNumber.charAt(j);
+
+                    if (!asciiart.containsKey(currentChar)) {
+                        throw new UnexpectedRomanException(currentChar);
+                    }
+
+                    romanAsciiart.append(asciiart.get(currentChar)[i]);
+                }
+                romanAsciiart.append('\n');
+            }
+        }
+
+        return romanAsciiart.toString();
     }
 }

9dfd80cc867a889be12e51991fd76c280b36d099
rmdevv <michelonriccardo@gmail.com>
2023-05-15 22:29:31 +0200

    Sviluppo del codice sorgente della classe IntegerToRoman close #7

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -5,9 +5,26 @@
 
 package it.unipd.mtss;
 
-public class IntegerToRoman {
-    public static String convert(int number) {
-        // TODO
-        return new String("");
+class IntegerToRoman {
+
+    private static final int[] VALUES = 
+    { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+    private static final String[] ROMAN_LETTERS = 
+    { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+    
+    public static String convert(int num) throws UnexpectedNumberException {
+        if (num < 0 || num > 1000) {
+            throw new UnexpectedNumberException();   
+        }
+        
+        String romanNum = "";
+        
+        for (int i = 0; i < VALUES.length; i++) {
+            while (num >= VALUES[i]) {
+                romanNum += ROMAN_LETTERS[i];
+                num -= VALUES[i];
+            }
+        }
+        return romanNum;
     }
 }
